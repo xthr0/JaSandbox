fbd96c10cc12c6d1b3097ddf45e856000e0fced9
Sirazhudin <the.r0.onneee@gmail.com>
2022-11-20 20:38:15 +0300

    Lmao

---
+++ NasaImage/src/main/java/Main.java
@@ -0,0 +1,42 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main {
+    public static final String URL = "https://api.nasa.gov/planetary/apod?api_key=";
+    //Enter your own API key from "https://api.nasa.gov/"
+    public static final ObjectMapper mapper = new ObjectMapper();
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
+                        .setSocketTimeout(30000)    // максимальное время ожидания получения данных
+                        .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
+                        .build())
+                .build();
+
+        CloseableHttpResponse response = httpClient.execute(new HttpGet(URL));
+
+        NasaObject nasaObject = mapper.readValue(response.getEntity().getContent(), NasaObject.class);
+        System.out.println(nasaObject);
+
+        CloseableHttpResponse picResponse = httpClient.execute(new HttpGet(nasaObject.getUrl()));
+
+        String[] arr = nasaObject.getUrl().split("/");
+        String file = arr[arr.length-1];
+
+        HttpEntity entity = picResponse.getEntity();
+        if (entity != null) {
+            try (FileOutputStream output = new FileOutputStream(file)){
+                entity.writeTo(output);
+            }
+        }
+    }
+}
---
+++ NasaImage/src/main/java/NasaObject.java
@@ -0,0 +1,78 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class NasaObject {
+    private final String copyright;
+    private final String date;
+    private final String explanation;
+    private final String hdurl;
+    private final String media_type;
+    private final String service_version;
+    private final String title;
+    private final String url;
+
+    public NasaObject(
+        @JsonProperty("copyright") String copyright,
+        @JsonProperty("date") String date,
+        @JsonProperty("explanation") String explanation,
+        @JsonProperty("hdurl") String hdurl,
+        @JsonProperty("media_type") String media_type,
+        @JsonProperty("service_version") String service_version,
+        @JsonProperty("title") String title,
+        @JsonProperty("url") String url)
+    {
+        this.copyright = copyright;
+        this.date = date;
+        this.explanation = explanation;
+        this.hdurl = hdurl;
+        this.media_type = media_type;
+        this.service_version = service_version;
+        this.title = title;
+        this.url = url;
+    }
+
+    public String getCopyright() {
+        return copyright;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getExplanation() {
+        return explanation;
+    }
+
+    public String getHdurl() {
+        return hdurl;
+    }
+
+    public String getMedia_type() {
+        return media_type;
+    }
+
+    public String getService_version() {
+        return service_version;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    @Override
+    public String toString() {
+        return "NasaJSON{" +
+                "copyright='" + copyright + '\'' +
+                ", date='" + date + '\'' +
+                ", explanation='" + explanation + '\'' +
+                ", hdurl='" + hdurl + '\'' +
+                ", media_type='" + media_type + '\'' +
+                ", service_version='" + service_version + '\'' +
+                ", title='" + title + '\'' +
+                ", url='" + url + '\'' +
+                '}';
+    }
+}
